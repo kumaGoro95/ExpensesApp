66dde510b2354bb911f878235dbb00e6933b0b34
kumaGoro95 <todira.island@gmail.com>
2020-12-19 22:08:00 +0900

    feat:メイン画面で出入金一覧を表示/投稿日時を自動取得

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -31,25 +34,32 @@ public class HomeController {
 	private final BCryptPasswordEncoder passwordEncoder;
 	
 	@GetMapping("/main")
-	public String main(Authentication loginUser, Model model){
-		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
+	public String main(@ModelAttribute MoneyRecord moneyRecord, Authentication loginUser, Model model){
+		SiteUser user = userRepository.findByUsername(loginUser.getName());
+		model.addAttribute("user", user);
+		model.addAttribute("records", moneyRecordRepository.findByUserId(user.getUserId()));
 		return "main";
 	}
 	
 	@GetMapping("/setting")
 	public String setting(Authentication loginUser, Model model){
-		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
+		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));	
 		return "setting";
 	}
 	
 	@PostMapping("/setting")
-	public String process(@Validated @ModelAttribute("user") SiteUser user, BindingResult result) {
+	public String process(@Validated @ModelAttribute("user") SiteUser user, BindingResult result, Authentication loginUser) {
 		//@Validatedで入力値チェック→BindingResultに結果が入る→result.hasErrors()でエラーがあるか確認
 		if(result.hasErrors()) { 
 			return "main";
 		}
-		//LocalDate a = LocalDate.parse(user.getDateOfBirth());
+		SiteUser currentUserInfo = userRepository.findByUsername(loginUser.getName());
+		user.setUserId(currentUserInfo.getUserId());
+		user.setRole(currentUserInfo.getRole());
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		user.setCreatedAt(currentUserInfo.getCreatedAt());
+		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+		user.setUpdatedAt(ldt);
 		userRepository.save(user);
 		
 		return "redirect:/main?setting";
@@ -69,10 +79,16 @@ public class HomeController {
 		}
 		SiteUser user = userRepository.findByUsername(loginUser.getName());
 		moneyRecord.setUserId(user.getUserId());
-		LocalDateTime ldt = LocalDateTime.now();
+		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+		
+		//ZonedDateTime zonedDateTime = ZonedDateTime.now();
+		//DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSSxxxxx VV");
+		//String s = zonedDateTime.format(formatter);
+		
 		moneyRecord.setCreatedAt(ldt);
 		moneyRecordRepository.save(moneyRecord);
 		
 		return "redirect:/main?post";
 	}
+
 }
