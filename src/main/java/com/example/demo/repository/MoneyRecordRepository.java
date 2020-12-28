c7fe04df4ac662b5c61f01e2a5a7a6e65c9a8f1c
kumaGoro95 <todira.island@gmail.com>
2020-12-28 16:02:07 +0900

    feat:月ごとの出入金を検索・一覧表示する機能を追加 #18

--- src/main/java/com/example/demo/repository/MoneyRecordRepository.java
+++ src/main/java/com/example/demo/repository/MoneyRecordRepository.java
@@ -1,13 +1,15 @@
 package com.example.demo.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.model.MoneyRecord;
-import com.example.demo.model.SiteUser;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface MoneyRecordRepository extends JpaRepository<MoneyRecord, Long> {
+import com.example.demo.model.MoneyRecord;
+
+public interface MoneyRecordRepository extends JpaRepository<MoneyRecord, Long>, JpaSpecificationExecutor<MoneyRecord>{
 	
 	public List<MoneyRecord> findByUsername(String username);
 	
@@ -16,5 +18,8 @@ public interface MoneyRecordRepository extends JpaRepository<MoneyRecord, Long> 
 	public void deleteByRecordId(Long recordId);
 	
 	public List<MoneyRecord> findByUsernameOrderByRecordDate(String username);
+	
+	public List<MoneyRecord> findByRecordDateBetweenOrderByRecordDateAsc(LocalDate start, LocalDate end);
+	
 	
 }
