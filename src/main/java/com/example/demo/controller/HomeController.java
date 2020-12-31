a4f4a1f5a2ee0fd417cb8ae1a448381b9e383515
kumaGoro95 <todira.island@gmail.com>
2020-12-31 17:38:56 +0900

    add: カテゴリ名一覧取得機能追加

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
-
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -12,14 +16,18 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.dao.CategoryDaoImpl;
+import com.example.demo.model.CategoryName;
 import com.example.demo.model.MoneyRecord;
 import com.example.demo.model.SiteUser;
 import com.example.demo.repository.SiteUserRepository;
 import com.example.demo.repository.MoneyRecordRepository;
 import com.example.demo.repository.CategoryRepository;
+import com.example.demo.dao.MoneyRecordDaoImpl;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,14 +38,26 @@ public class HomeController {
 	//DI
 	private final SiteUserRepository userRepository;
 	private final MoneyRecordRepository moneyRecordRepository;
+	private MoneyRecordDaoImpl mrDao;
+	private CategoryDaoImpl cDao;
 	private final CategoryRepository categoryRepository;
 	private final BCryptPasswordEncoder passwordEncoder;
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	@PostConstruct
+	public void init() {
+		mrDao = new MoneyRecordDaoImpl(em);
+		cDao = new CategoryDaoImpl(em);
+	}
 	
 	@GetMapping("/main")
 	public String main(@ModelAttribute MoneyRecord moneyRecord, Authentication loginUser, Model model){
 		SiteUser user = userRepository.findByUsername(loginUser.getName());
 		model.addAttribute("user", user);
-		model.addAttribute("records", moneyRecordRepository.findByUserId(user.getUserId()));
+		model.addAttribute("records", moneyRecordRepository.findByUsernameOrderByRecordDate(loginUser.getName()));
+		model.addAttribute("temporaryRecords", mrDao.find(loginUser.getName(), "2020-12"));
 		return "main";
 	}
 	
@@ -53,11 +73,7 @@ public class HomeController {
 		if(result.hasErrors()) { 
 			return "main";
 		}
-		SiteUser currentUserInfo = userRepository.findByUsername(loginUser.getName());
-		user.setUserId(currentUserInfo.getUserId());
-		user.setRole(currentUserInfo.getRole());
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		user.setCreatedAt(currentUserInfo.getCreatedAt());
 		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 		user.setUpdatedAt(ldt);
 		userRepository.save(user);
@@ -65,22 +81,36 @@ public class HomeController {
 		return "redirect:/main?setting";
 	}
 	
+	@Transactional
+	@GetMapping("/deleteUser")
+	public String deleteUser(Authentication loginUser) {
+		userRepository.deleteByUsername(loginUser.getName());
+		
+		return "login";
+	}
+	
 	@GetMapping("/post")
 	public String post(@ModelAttribute("moneyRecord") MoneyRecord moneyRecord, Authentication loginUser, Model model){
 		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
-		model.addAttribute("categories", categoryRepository.findAll());
+		model.addAttribute("subcategories", categoryRepository.findAll());
+		List<CategoryName> list = cDao.getCategory();
+		model.addAttribute("categories", list);
+		
+		for(int i = 0; i < list.size(); i++) {
+			CategoryName a = list.get(i);
+			System.out.println(a.getCategoryName());
+			System.out.println(a.getNumberOfSubcategory());
+		}
 		return "post";
 	}
 	
 	@PostMapping("/post")
 	public String process(@Validated @ModelAttribute("moneyRecord") MoneyRecord moneyRecord, BindingResult result, Authentication loginUser) {
-		//@Validatedで入力値チェック→BindingResultに結果が入る→result.hasErrors()でエラーがあるか確認
-		if(result.hasErrors()) { 
-			return "post";
+		if(result.hasErrors()) {
+			System.out.println(result);
+			System.out.println(moneyRecord.getNote());
+			return "redirect:/post?post";
 		}
-		//moneyRecord.setCategoryId();
-		SiteUser user = userRepository.findByUsername(loginUser.getName());
-		moneyRecord.setUserId(user.getUserId());
 		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 		
 		//ZonedDateTime zonedDateTime = ZonedDateTime.now();
@@ -91,6 +121,45 @@ public class HomeController {
 		moneyRecordRepository.save(moneyRecord);
 		
 		return "redirect:/main?post";
+	}
+	
+	@RequestMapping("/record/{recordId}")
+	public String showRecord(@PathVariable("recordId") Long recordId, Model model){
+		model.addAttribute("record", moneyRecordRepository.findByRecordId(recordId));
+		
+		return "record";
+	}
+	
+	@Transactional
+	@GetMapping("/deleteRecord/{recordId}")
+	public String deleteRecord(@PathVariable("recordId") Long recordId, Model model) {
+		moneyRecordRepository.deleteById(recordId);
+		
+		return "redirect:/main?record";
+	}
+	
+	@GetMapping("/editRecord/{recordId}")
+	public String editRecord(@PathVariable("recordId") Long recordId, Authentication loginUser, Model model){
+		model.addAttribute("record", moneyRecordRepository.findByRecordId(recordId));
+		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
+		model.addAttribute("categories", categoryRepository.findAll());
+		
+		return "recordEdit";
+	}
+	
+	@PostMapping("/editRecord/{recordId}")
+	public String updateRecord(@Validated @ModelAttribute("moneyRecord") MoneyRecord moneyRecord, BindingResult result, Authentication loginUser) {
+		if(result.hasErrors()) {
+			return "main";
+		}
+
+		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+		moneyRecord.setUpdatedAt(ldt);
+
+		
+		moneyRecordRepository.save(moneyRecord);
+		
+		return "redirect:/main?editRecord";
 	}
 
 }
--- src/main/java/com/example/demo/controller/SecurityController.java
+++ src/main/java/com/example/demo/controller/SecurityController.java
@@ -3,6 +3,10 @@ package com.example.demo.controller;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -13,8 +17,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.dao.MoneyRecordDaoImpl;
 import com.example.demo.model.SiteUser;
 import com.example.demo.repository.MoneyRecordRepository;
+
 import com.example.demo.repository.SiteUserRepository;
 import com.example.demo.util.Role;
 
@@ -23,47 +29,58 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 public class SecurityController {
-	
-	//DI
+
+	// DI
 	private final SiteUserRepository userRepository;
-	private final MoneyRecordRepository moneyRecordRepository;
+	private MoneyRecordDaoImpl mrDao;
 	private final BCryptPasswordEncoder passwordEncoder;
+
+	@PersistenceContext
+	EntityManager em;
+
+	@PostConstruct
+	public void init() {
+		mrDao = new MoneyRecordDaoImpl(em);
+	}
 
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
-	
+
 	@GetMapping("/")
-	//Authentication・・・認証済みのユーザー情報を取得
+	// Authentication・・・認証済みのユーザー情報を取得
 	public String loginProcess(Authentication loginUser, Model model) {
-		model.addAttribute("records", moneyRecordRepository.findByUsername(loginUser.getName()));
+		model.addAttribute("records", mrDao.findByUsername(loginUser.getName()));
+		//model.addAttribute("records", moneyRecordRepository.findByUsernameOrderByRecordDate(loginUser.getName()));
 		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
+		model.addAttribute("temporaryRecords", mrDao.find(loginUser.getName(), "2020-12"));
+
 		return "main";
 	}
-	
+
 	@GetMapping("/register")
-	public String register(@ModelAttribute("user") SiteUser user){
+	public String register(@ModelAttribute("user") SiteUser user) {
 		return "register";
 	}
-	
+
 	@PostMapping("/register")
 	public String process(@Validated @ModelAttribute("user") SiteUser user, BindingResult result) {
-		//@Validatedで入力値チェック→BindingResultに結果が入る→result.hasErrors()でエラーがあるか確認
+		// @Validatedで入力値チェック→BindingResultに結果が入る→result.hasErrors()でエラーがあるか確認
 		user.setUserNickname(user.getUsername());
-		if(result.hasErrors()) { 
+		if (result.hasErrors()) {
 			return "register";
 		}
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		if(user.isAdmin()) {
+		if (user.isAdmin()) {
 			user.setRole(Role.ADMIN.name());
-		}else {
+		} else {
 			user.setRole(Role.USER.name());
 		}
 		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 		user.setCreatedAt(ldt);
 		userRepository.save(user);
-		
+
 		return "redirect:/login?register";
 	}
 }
