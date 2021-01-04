cb4893d426689a994391de14abe539b03e876ada
kumaGoro95 <todira.island@gmail.com>
2021-01-04 18:18:47 +0900

    add:投稿編集・削除機能を追加 #31

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -42,124 +41,131 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 public class PostController {
-	
-	//DI
+
+	// DI
 	private final SiteUserRepository userRepository;
 	private final PostRepository postRepository;
 	private final PostCommentRepository commentRepository;
 	private final LikeRepository likeRepository;
-	
-	
-	//投稿系ホーム画面
+
+	// 投稿系ホーム画面
 	@GetMapping("/postmain")
-	public String goToPost(@ModelAttribute("posts") Post post, Authentication loginUser, Model model){
+	public String goToPost(@ModelAttribute("posts") Post post, Authentication loginUser, Model model) {
 		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
 		model.addAttribute("posts", postRepository.findAll());
-		
+
 		return "postmain";
 	}
-	
-	//投稿画面へ遷移
+
+	// 投稿画面へ遷移
 	@GetMapping("/post")
-	public String post(@ModelAttribute("post") Post post, Authentication loginUser, Model model){
+	public String post(@ModelAttribute("post") Post post, Authentication loginUser, Model model) {
 		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
-		
+
 		return "post";
 	}
-	
-	//投稿実行
+
+	// 投稿実行
 	@PostMapping("/post")
 	public String post(@Validated @ModelAttribute("post") Post post, BindingResult result, Authentication loginUser) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			System.out.println(result);
 			System.out.println(post.getPostBody());
 			return "redirect:/post?post";
 		}
 		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-		
-		//ZonedDateTime zonedDateTime = ZonedDateTime.now();
-		//DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSSxxxxx VV");
-		//String s = zonedDateTime.format(formatter);
-		
+
+		// ZonedDateTime zonedDateTime = ZonedDateTime.now();
+		// DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd
+		// HH:mm:ss.SSSxxxxx VV");
+		// String s = zonedDateTime.format(formatter);
+
 		post.setCreatedAt(ldt);
 		postRepository.save(post);
-		
+
 		return "redirect:/postmain?recordPost";
 	}
-	
-	//投稿内容詳細画面へ遷移
+
+	// 投稿内容詳細画面へ遷移
 	@RequestMapping("/post/{postId}")
-	public String showPost(@PathVariable("postId") Long postId, @ModelAttribute("comment") PostComment comment, Authentication loginUser, Model model){
+	public String showPost(@PathVariable("postId") Long postId, @ModelAttribute("comment") PostComment comment,
+			Authentication loginUser, Model model) {
 		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
 		model.addAttribute("post", postRepository.findByPostId(postId));
 		model.addAttribute("comments", commentRepository.findByPostId(postId));
-		model.addAttribute("theNumberOfLikes",likeRepository.countByPostId(postId));
+		model.addAttribute("theNumberOfLikes", likeRepository.countByPostId(postId));
 		
+
 		return "postdetail";
 	}
-	
-	//コメント投稿を実行
+
+	// コメント投稿を実行
 	@PostMapping("/postComment")
-	public String postComment(@Validated @ModelAttribute("comment") PostComment comment, BindingResult result, Authentication loginUser) {
-		if(result.hasErrors()) {
+	public String postComment(@Validated @ModelAttribute("comment") PostComment comment, BindingResult result,
+			Authentication loginUser) {
+		if (result.hasErrors()) {
 			System.out.println(result);
 			System.out.println(comment.getCommentBody());
 			return "redirect:/post?post";
 		}
 		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-		
-		//ZonedDateTime zonedDateTime = ZonedDateTime.now();
-		//DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSSxxxxx VV");
-		//String s = zonedDateTime.format(formatter);
-		
+
+		// ZonedDateTime zonedDateTime = ZonedDateTime.now();
+		// DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd
+		// HH:mm:ss.SSSxxxxx VV");
+		// String s = zonedDateTime.format(formatter);
+
 		comment.setCreatedAt(ldt);
 		commentRepository.save(comment);
-		
+
 		return "redirect:/postmain?postdetail";
 	}
-	
-	//いいね実行
+
+	// いいね実行
 	@RequestMapping("/like/{postId}")
-	public String Like(@PathVariable("postId") Long postId, @ModelAttribute("like") Like like, Authentication loginUser, Model model) {
+	public String Like(@PathVariable("postId") Long postId, @ModelAttribute("like") Like like, Authentication loginUser,
+			Model model) {
 		like.setPostId(postId);
 		like.setUsername(loginUser.getName());
 		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 		like.setCreatedAt(ldt);
 		likeRepository.save(like);
-		
+
 		return "redirect:/postmain?postdetail";
 	}
-	
-	//いいね一覧を表示
+
+	// いいね一覧を表示
 	@RequestMapping("/likesList/{postId}")
-	public String showLikes(@PathVariable("postId") Long postId, @ModelAttribute("likes") Like like, Authentication loginUser, Model model) {
+	public String showLikes(@PathVariable("postId") Long postId, @ModelAttribute("likes") Like like,
+			Authentication loginUser, Model model) {
 		model.addAttribute("likes", likeRepository.findByPostId(postId));
-		
+
 		return "likesDetail";
 	}
-	
-	//コメント削除
+
+	// コメント削除
 	@Transactional
 	@GetMapping("/deleteComment/{commentId}")
 	public String deleteComment(@PathVariable("commentId") Long commentId, Model model) {
-		commentRepository.deleteById(commentId);
-		
+		commentRepository.deleteByCommentId(commentId);
+
 		return "redirect:/postmain?postdetail";
 	}
-	
-	//コメント編集画面へ遷移
+
+	// コメント編集画面へ遷移
 	@GetMapping("/editComment/{commentId}")
-	public String editComment(@PathVariable("commentId") Long commentId, Authentication loginUser, Model model){
+	public String editComment(@PathVariable("commentId") Long commentId, Authentication loginUser, Model model) {
 		model.addAttribute("comment", commentRepository.findByCommentId(commentId));
 		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
-		
+
 		return "commentEdit";
 	}
-	
-	//コメント編集を実行
+
+	// コメント編集を実行
 	@PostMapping("/updateComment")
-	public String updateComment(@Validated @ModelAttribute("comment") PostComment comment, BindingResult result, Authentication loginUser) {
-		if(result.hasErrors()) {
+	public String updateComment(@Validated @ModelAttribute("comment") PostComment comment, BindingResult result,
+			Authentication loginUser) {
+		if (result.hasErrors()) {
 			return "main";
 		}
 
@@ -167,11 +173,44 @@ public class PostController {
 		System.out.println(ldt);
 		comment.setUpdatedAt(ldt);
 
-		
 		commentRepository.save(comment);
-		
+
 		return "redirect:/postmain?commentEdit";
 	}
-	
+
+	// 投稿削除
+	@Transactional
+	@GetMapping("/deletePost/{postId}")
+	public String deletePost(@PathVariable("postId") Long postId, Model model) {
+		postRepository.deleteByPostId(postId);
+
+		return "redirect:/postmain?postdetail";
+	}
+
+	// 投稿編集画面へ遷移
+	@GetMapping("/editPost/{postId}")
+	public String editPost(@PathVariable("postId") Long postId, Authentication loginUser, Model model) {
+		model.addAttribute("post", postRepository.findByPostId(postId));
+		model.addAttribute("user", userRepository.findByUsername(loginUser.getName()));
+
+		return "postEdit";
+	}
+
+	// 投稿編集を実行
+	@PostMapping("/updatePost")
+	public String updatePost(@Validated @ModelAttribute("post") Post post, BindingResult result,
+			Authentication loginUser) {
+		if (result.hasErrors()) {
+			return "main";
+		}
+
+		LocalDateTime ldt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+		System.out.println(ldt);
+		post.setUpdatedAt(ldt);
+
+		postRepository.save(post);
+
+		return "redirect:/postmain?postEdit";
+	}
 
 }
